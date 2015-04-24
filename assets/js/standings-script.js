75c898436c85b2c7db0b4d9f05e75e1a9f6075fd
efossier <evanfossier@berkeley.edu>
2015-04-23 18:42:44 -0700

    started standings page, implemented the division selector

---
+++ assets/js/standings-script.js
@@ -0,0 +1,30 @@
+/*
+    Javascript for the standings page
+ */
+
+// GLOBAL VARIABLES
+var $CUR_DIVISION; //contains the jquery element of the currently selected division
+
+$(document).ready(function(){
+    $CUR_DIVISION = $(".div-selected");
+    console.log("Current division = "+$CUR_DIVISION.html());
+
+    $(".div-button").click(function(){
+        move_div_selector($(this));
+    });
+});
+
+function move_div_selector($elem){
+    if($CUR_DIVISION == $elem) return;
+
+    var title = $elem.html();
+    console.log("moving to division:"+title);
+
+    $CUR_DIVISION.removeClass("div-selected");
+    $elem.addClass("div-selected");
+    $("#division-selector").removeClass($CUR_DIVISION.html().toLowerCase());
+    $("#division-selector").addClass(title.toLowerCase());
+    $CUR_DIVISION = $elem;
+
+
+}
