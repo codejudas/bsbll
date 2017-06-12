78b698c7a3cb773e4cf9146d4f693ac384eb8817
Evan Fossier <evan.fossier@yahoo.com>
2017-06-11 18:57:34 -0700

    Tidy up webpack config

--- v2/webpack.config.js
+++ v2/webpack.config.js
@@ -6,17 +6,37 @@ const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
 const production = process.env.NODE_ENV === 'production';
 
-function devConfig() {
-  return {
-    cache: true,
-    entry: './src/web/app.jsx',
-    devtool: "source-map", // enable sourcemap for debugging
+const PROD_CONFIG = {
+  cache: true,
+  entry: './src/web/app.jsx',
+  devtool: "source-map", // enable sourcemap for debugging
+  output: {
+    path: path.resolve(__dirname, 'build/web'),
+    publicPath: '/assets/js/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // All files with a '.js' or '.jsx' extension will be handled by 'babel'.
+      { test: /\.jsx?/, loaders: ['babel-loader'], exclude: /node_modules/ },
+      { test: /\.scss/, use: [ 'style-loader', 'css-loader', 'sass-loader' ]},
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/web/index.template.ejs',
+      filename: 'index.html',
+      inject: 'body',
+      alwaysWriteToDisk: true,
+    })
+  ]
+};
+
+const DEV_CONFIG = Object.assign({}, PROD_CONFIG, {
     target: 'web', // default value for something
-    output: {
-      path: path.resolve(__dirname, 'build/web'),
-      publicPath: '/assets/js/',
-      filename: 'bundle.js'
-    },
     module: {
       rules: [
         // All files with a '.js' or '.jsx' extension will be handled by 'babel'.
@@ -25,10 +45,9 @@ function devConfig() {
 
         // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
         { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
+      ],
     },
     plugins: [
-      // HMR
       new webpack.HotModuleReplacementPlugin(),
       new HtmlWebpackPlugin({
         template: './src/web/index.template.ejs',
@@ -36,47 +55,7 @@ function devConfig() {
         inject: 'body',
         alwaysWriteToDisk: true,
       }),
-      // Necessary to write out index.html w/ HMR
-      new HtmlWebpackHarddiskPlugin({
-        outputPath: path.resolve(__dirname, 'build/web')
-      })
     ]
-  };
-}
+});
 
-function prodConfig() {
-  return {
-    cache: true,
-    entry: './src/web/app.jsx',
-    devtool: "source-map", // enable sourcemap for debugging
-    output: {
-      path: path.resolve(__dirname, 'build/web'),
-      publicPath: '/assets/js/',
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        // All files with a '.js' or '.jsx' extension will be handled by 'babel'.
-        { test: /\.jsx?/, loaders: ['babel-loader'], exclude: /node_modules/ },
-        { test: /\.scss/, use: [ 'style-loader', 'css-loader', 'sass-loader' ]},
-
-        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/web/index.template.ejs',
-        filename: 'index.html',
-        inject: 'body',
-        alwaysWriteToDisk: true,
-      })
-    ]
-  };
-}
-
-if (production) {
-  module.exports = prodConfig();
-} else {
-  module.exports = devConfig();
-}
+module.exports = production ? PROD_CONFIG : DEV_CONFIG;
